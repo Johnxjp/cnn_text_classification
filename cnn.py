5f8d853c4ac52797f9fc00990cb0d58969463bd5
John Lingi <john.lingi@babylonhealth.com>
2019-09-19 16:12:51 +0100

    Copied cnn implementation from other project

---
+++ cnn.py
@@ -0,0 +1,76 @@
+"""
+Contains code for CNN classifiers.
+Move this code into ir-research.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class YKCNN(nn.Module):
+    """Yoon Kim CNN model without the Fully connected classification layer"""
+
+    def __init__(
+        self,
+        vocabulary_size,
+        max_seq_length,
+        embed_dim=300,
+        padding_idx=0,
+        kernel_heights=[3, 4, 5],
+        dropout=0,
+        add_embedding_layer=True,
+    ):
+        super().__init__()
+        self.n_out_channels = 100
+        self.n_kernels = len(kernel_heights)
+        self.pool_sizes = [(max_seq_length - K, 1) for K in kernel_heights]
+        self.max_seq_length = max_seq_length
+
+        if add_embedding_layer:
+            self.embedding = nn.Embedding(
+                vocabulary_size, embed_dim, padding_idx=0
+            )
+        else:
+            self.embedding = None
+        self.dropout = nn.Dropout(dropout)
+        self.convs = nn.ModuleList(
+            [
+                nn.Conv2d(1, self.n_out_channels, kernel_size=(K, embed_dim))
+                for K in kernel_heights
+            ]
+        )
+        self.pools = nn.ModuleList(
+            [
+                nn.MaxPool2d(kernel_size=pool_size)
+                for pool_size in self.pool_sizes
+            ]
+        )
+
+
+    def forward(self, x):
+        """
+        x: (batch_size, max_sequence_length)
+        """
+        batch_size = x.size(0)
+        assert x.size(1) == self.max_seq_length
+        self.train()
+
+        if self.embedding is not None:
+            x = self.embedding(x)
+        # adds input channel
+        # x = (batch, n_input_channel, max_seq, embedding_dim)
+        x = x.unsqueeze(dim=1)
+
+        # Output from cnn with above layers will be
+        # (batch_size, out * n_kernels, 1, 1)
+        concated_tensor = torch.zeros(
+            (batch_size, self.n_out_channels * self.n_kernels, 1, 1)
+        )
+        for i, (convs, pools) in enumerate(zip(self.convs, self.pools)):
+            activation = pools(F.relu(convs(x)))
+            concated_tensor[
+                :, i * self.n_out_channels: (i + 1) * self.n_out_channels, :
+            ] = activation
+
+        x = concated_tensor.view(concated_tensor.shape[0], -1)
+        return self.dropout(x)
