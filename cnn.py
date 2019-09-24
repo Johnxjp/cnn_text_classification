5a6576d5e676e0c84ee0c4eed15f907c0cf0186b
John Lingi <lingijohn23@gmail.com>
2019-09-25 00:05:03 +0100

    Fixed bug with missing in_channels

--- cnn.py
+++ cnn.py
@@ -13,7 +13,6 @@ class YKCNNClassifier(nn.Module):
         vocabulary_size,
         max_seq_length,
         output_dims=2,
-        in_channels=1,
         out_channels=100,
         embed_dim=300,
         padding_idx=0,
@@ -23,7 +22,7 @@ class YKCNNClassifier(nn.Module):
     ):
         super().__init__()
         self.out_channels = out_channels
-        self.in_channels = in_channels
+        self.in_channels = 1
         self.n_kernels = len(kernel_heights)
         self.pool_sizes = [(max_seq_length - K, 1) for K in kernel_heights]
         self.max_seq_length = max_seq_length
@@ -32,8 +31,8 @@ class YKCNNClassifier(nn.Module):
             vocabulary_size, embed_dim, padding_idx=0
         )
         if embedding_matrix is not None:
-            # Load pre-trained weights
-            self.embedding.weight()
+            # Load pre-trained weights. Should be torch FloatTensor
+            self.embedding.from_pretrained(embedding_matrix)
 
         self.dropout = nn.Dropout(dropout)
         self.convs = nn.ModuleList(
@@ -59,29 +58,25 @@ class YKCNNClassifier(nn.Module):
         x: (batch_size, max_sequence_length)
         """
         batch_size = x.size(0)
-        using_gpu = x.is_cuda
         assert x.size(1) == self.max_seq_length
 
+        # (batch, max_sequenece_length, embedding_dim)
         x = self.embedding(x)
 
         # adds input channel
-        # x = (batch, n_input_channel, max_seq, embedding_dim)
+        # (batch, 1, max_sequence_length, embedding_dim)
         x = x.unsqueeze(dim=1)
+
+        out_tensors = []
+        for i, (convs, pools) in enumerate(zip(self.convs, self.pools)):
+            activation = pools(F.relu(convs(x)))
+            out_tensors.append(activation)
 
         # Output from conv and pooling operation will be of size
         # (batch_size, out * n_kernels, 1, 1)
-        concated_tensor = torch.zeros(
-            (batch_size, self.out_channels * self.n_kernels, 1, 1)
-        )
-        if using_gpu:
-            concated_tensor = concated_tensor.cuda()
-        for i, (convs, pools) in enumerate(zip(self.convs, self.pools)):
-            activation = pools(F.relu(convs(x)))
-            concated_tensor[
-                :, i * self.out_channels : (i + 1) * self.out_channels, :
-            ] = activation
+        x = torch.cat(out_tensors, dim=1)
 
         # Reshape to pass into fully connected
-        x = concated_tensor.view(concated_tensor.shape[0], -1)
+        x = x.view(batch_size, -1)
         x = self.dropout(x)
         return self.fc(x)
